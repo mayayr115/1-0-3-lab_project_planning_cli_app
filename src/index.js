d8fac7a92d88e11b2288a2a0bfcfc94c544f3da9
mayayr115 <mayaramkishuncsh2027@gmail.com>
2023-10-22 22:49:15 -0400

    Final changes to code and syntax before reformatting

--- src/index.js
+++ src/index.js
@@ -4,29 +4,23 @@ let name = ''
 
 // small, helper functions
 const welcomeAndGetName = () => {
-  console.log('Welcome!')
-  name = prompt('What is your name: ')
-  console.log(`Hello ${name.trim()}, nice to meet you!\n`)
-}
+  console.log('Welcome!');
+  name = prompt('What is your name: ');
+  console.log(`Hello ${name.trim()}, nice to meet you!\n`);
+};
 
-const sayGoodbye = () => {console.log(`Goodbye, ${name}!`)}
+const sayGoodbye = () => {console.log(`Goodbye, ${name}!`);};
 
-const showOptions = () => {
-  console.log('Here are your options:\n',
-  '1 - play a guessing game\n',
-  '2 - words of wisdom\n',
-  '3 - cheer you on!\n',
-  'Any other key - exit\n')
-}
+const showOptions = () => {console.log('Here are your options:\n1 - play a guessing game\n2 - words of wisdom\n3 - cheer you on!\nAny other key - exit\n');};
 
-const getRandomIntInRange = (min, max) => Math.floor(Math.random() * max) + min
+const getRandomIntInRange = (min, max) => Math.floor(Math.random() * max) + min;
 
 const handleGuessingGame = () => {
-  const getUserNumber = Number(prompt('Pick a number: '))
-  const rightNumber = getRandomIntInRange(0, 11)
-  if (getUserNumber === rightNumber) return `Congrats ${name}! You Win!`
-  if (getUserNumber !== rightNumber) return `Sorry ${name}. Better luck next time.\nThe number was: ${rightNumber}`
-}
+  const getUserNumber = Number(prompt('Pick a number: '));
+  const rightNumber = getRandomIntInRange(0, 11);
+  if (getUserNumber === rightNumber) return `Congrats ${name}! You Win!`;
+  if (getUserNumber !== rightNumber) return `Sorry ${name}. Better luck next time.\nThe number was: ${rightNumber}`;
+};
 
 const wordsOfWisdom = [
   'You should go to bed by 12:30',
@@ -35,7 +29,7 @@ const wordsOfWisdom = [
   "The best laid plans often fail, but plans that don't exist can never succeed",
   'Time management and communication are the two most important skills in the workplace',
   'Try to listen more than you speak',
-]
+];
 
 const cheers = [
   'You can do it!',
@@ -51,17 +45,17 @@ const getCheer = () => cheers[getRandomIntInRange(0, cheers.length)];
 
 // more complex event handler functions
 const chooseOption = () => {
-  showOptions()
-  const option = Number(prompt('What is your option: ').trim())
-  if (option === 1) return handleGuessingGame();
-  if (option === 2) return getWordsOfWisdom();
-  if (option === 3) return getCheer();
+  showOptions();
+  const option = Number(prompt('What is your option: ').trim());
+  if (option === 1) console.log(handleGuessingGame());
+  if (option === 2) console.log(getWordsOfWisdom());
+  if (option === 3) console.log(getCheer());
 }
 
 // Primary runner function
 const main = () => {
-  welcomeAndGetName()
-  console.log(chooseOption())
-  sayGoodbye()
+  welcomeAndGetName();
+  chooseOption();
+  sayGoodbye();
 }
 main();
